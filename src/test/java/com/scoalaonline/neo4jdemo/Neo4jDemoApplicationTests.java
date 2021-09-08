81a7a3c04fef29945a9bad20ee9e913d2aba7750
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2021-09-08 18:34:58 +0300

    Initial commit

---
+++ src/test/java/com/scoalaonline/neo4jdemo/Neo4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoalaonline.neo4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
